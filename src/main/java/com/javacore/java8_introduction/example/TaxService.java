a84b103cec3d5370c96f48bd3654e40bebf0c35b
TomZheng <1219.zheng@gmail.com>
2022-10-17 08:11:01 -0700

    update

--- src/main/java/com/javacore/java8_introduction/example/TaxService.java
+++ src/main/java/com/javacore/java8_introduction/example/TaxService.java
@@ -2,8 +2,31 @@ package com.javacore.java8_introduction.example;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import org.testng.annotations.Test;
 
 public class TaxService {
+
+	@Test
+	void test1() {
+		System.out.println(evaluateTaxUsers1("non tax"));
+	}
+	private List<Employee> evaluateTaxUsers1(String input) {
+		if (input.equalsIgnoreCase("tax")) {
+			return DataBase.getEmployees()
+					.stream()
+					.filter(emp -> emp.getSalary() > 500000)
+					.collect(Collectors.toList());
+		} else {
+			return DataBase.getEmployees()
+					.stream()
+					.filter(emp -> emp.getSalary() < 500000)
+					.collect(Collectors.toList());
+		}
+	}
+
+
+
+
 
 	public static List<Employee> evaluateTaxUsers(String input) {
 		return (input.equalsIgnoreCase("tax"))
